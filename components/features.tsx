7dc4ef794d3b61db4ec1a97aa8fdc6e264728c9b
Santiago Carmuega <santiago@carmuega.me>
2023-04-20 09:08:31 -0300

    feat: Add rationale to homepage

--- components/features.tsx
+++ components/features.tsx
@@ -1,29 +1,16 @@
 import React from "react";
 
-function Card() {
+function Card(props: { title: string; description: string }) {
   return (
-    <div className="w-full h-full bg-white shadow-lg rounded-lg p-5 dark:bg-slate-900">
+    <div className="w-full h-full bg-white shadow-lg rounded-lg p-5 dark:bg-gray-800">
       <div className="flex items-center gap-x-4 mb-3">
-        <div className="inline-flex justify-center items-center w-[62px] h-[62px] rounded-full border-4 border-blue-50 bg-blue-100 dark:border-blue-900 dark:bg-blue-800">
-          <svg
-            className="w-6 h-6 text-blue-600 dark:text-blue-400"
-            xmlns="http://www.w3.org/2000/svg"
-            width="16"
-            height="16"
-            fill="currentColor"
-            viewBox="0 0 16 16"
-          >
-            <path d="M8 5a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3zm4 3a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3zM5.5 7a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm.5 6a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3z" />
-            <path d="M16 8c0 3.15-1.866 2.585-3.567 2.07C11.42 9.763 10.465 9.473 10 10c-.603.683-.475 1.819-.351 2.92C9.826 14.495 9.996 16 8 16a8 8 0 1 1 8-8zm-8 7c.611 0 .654-.171.655-.176.078-.146.124-.464.07-1.119-.014-.168-.037-.37-.061-.591-.052-.464-.112-1.005-.118-1.462-.01-.707.083-1.61.704-2.314.369-.417.845-.578 1.272-.618.404-.038.812.026 1.16.104.343.077.702.186 1.025.284l.028.008c.346.105.658.199.953.266.653.148.904.083.991.024C14.717 9.38 15 9.161 15 8a7 7 0 1 0-7 7z" />
-          </svg>
-        </div>
         <div className="flex-shrink-0">
           <h3 className="block text-lg font-semibold text-gray-800 dark:text-white">
-            TODO Feature
+            {props.title}
           </h3>
         </div>
       </div>
-      <p className="text-gray-600 dark:text-gray-400">TODO Description</p>
+      <p className="text-gray-600 dark:text-gray-400">{props.description}</p>
     </div>
   );
 }
@@ -32,12 +19,38 @@ export function Features(props: {}) {
   return (
     <div className="max-w-[85rem] px-4 py-10 sm:px-6 lg:px-8 lg:py-14 mx-auto">
       <div className="grid sm:grid-cols-2 lg:grid-cols-3 items-center gap-6 md:gap-10">
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
+        <Card
+          title="Specification"
+          description="A comment set of contracts, agnostic of any particular API provider or client tooling. A strict versioning system to deal with breaking changes."
+        />
+        <Card
+          title="SDKs"
+          description="A rich set SDKs for many mainstream languages such as Go, Rust, Python, JS, C++ and more, lowering the entry barrier for devs of any ecosystem."
+        />
+        <Card
+          title="Documentation"
+          description="Extensive documentation of all interfaces and structures to streamline the integration process."
+        />
+        <Card
+          title="Performance"
+          description="A serialized binary format which is compact and efficient, resulting in smaller message sizes and reduced network overhead compared to its JSON counterpart"
+        />
+        <Card
+          title="Event-Driven"
+          description="An interface that embraces the asynchronous nature of blockchains by leveraging event-driven integration patterns."
+        />
+        <Card
+          title="Proto3"
+          description="A versatile IDL that facilitates reliable communication between systems while promoting cross-language compatibility and ease of use."
+        />
+        <Card
+          title="Interoperability"
+          description="A common interface simplifies integrations between parties and avoids the common pitfall of vendor lock-in."
+        />
+        <Card
+          title="Open Governance"
+          description="A Technical Steering Committee (TSC) defined by the level of involvement in the source-code. Maintainers of the code decide how to evolve the protocol."
+        />
       </div>
     </div>
   );
