b56a927e78349a214db67ed13661daf6d65986e1
Santiago Carmuega <santiago@carmuega.me>
2023-04-21 09:21:41 -0300

    docs: Improve sequence diagrams

--- theme.config.jsx
+++ theme.config.jsx
@@ -1,9 +1,32 @@
 export default {
     logo: <img style={{"height": "30px"}} src="logo-dark.svg" />,
     project: {
-      link: 'https://github.com/shuding/nextra',
+      link: "https://github.com/utxorpc",
     },
     sidebar: {
       defaultMenuCollapseLevel: 0
-    }
+    },    
+    chat: {
+        link: "https://discord.gg/Vc3x8N9nz2",
+    },
+    docsRepositoryBase: "https://github.com/utxorpc/docs/blob/main",
+    useNextSeoProps() {
+        return {
+            titleTemplate: "%s – UTxO RPC",
+            description: "gRPC interface for UTxO blockchains",
+            canonical: "https://utxorpc.org",
+            siteName: "UTxO RPC",
+            openGraph: {
+                url: "https://utxorpc.org",
+                title: "UTxO RPC",
+                description: "gRPC interface for UTxO blockchains",
+            },
+            twitter: {
+                handle: "@utxorpc",
+            },
+        };
+    },
+    footer: {
+        text: "UTxO RPC",
+    },
   }
