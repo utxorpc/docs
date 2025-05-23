aad2454686a2c76d7bb3b0ecc2b5d427e642585f
Lance Vincent Salera <salera.lancevincent@gmail.com>
2025-05-23 19:03:01 +0800

    refactor: restructure nextra configuration for clarity and maintainability

--- next.config.js
+++ next.config.js
@@ -1,10 +1,21 @@
 import { remarkMermaid } from "remark-mermaid-nextra";
 import nextra from "nextra";
 
-export default nextra({
-  theme: "nextra-theme-docs",
-  themeConfig: "./theme.config.jsx",
-  mdxOptions: {
-    remarkPlugins: [remarkMermaid],
-  },
-});
+const withNextra = nextra({
+  theme: 'nextra-theme-docs',
+  themeConfig: './theme.config.jsx',
+  mdxOptions: { remarkPlugins: [remarkMermaid] }
+})
+
+/** @type {import('next').NextConfig} */
+export default withNextra({
+  trailingSlash: true,
+  async rewrites() {
+    return [
+      {
+        source: '/grpcui/:path*',
+        destination: 'http://127.0.0.1:8081/:path*'
+      },
+    ]
+  }
+})
